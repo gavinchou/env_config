29da0ccc3afbba044a302e00e8ea6e0ad452af48
Gavin Chou <gavineaglechou@gmail.com>
2016-06-04 00:52:56 +0800

    Add git config to install script of mac

--- mac_install.sh
+++ mac_install.sh
@@ -16,10 +16,12 @@ mv .bash_profile .bash_profile.`digitaldatetime`2>/dev/null
 mv .bashrc .bashrc.`digitaldatetime`2>/dev/null
 mv .inputrc .inputrc.`digitaldatetime`2>/dev/null
 mv git-completion.bash git-completion.bash.`digitaldatetime`2>/dev/null
+mv .gitconfig .gitconfig.`digitaldatetime`2>/dev/null
 
-ln -s ${env_config}/.bash_profile_mac .bash_profile
-ln -s ${env_config}/.bashr_mac .bashrc
-ln -s ${env_config}/.inputrc_mac .inputrc
+ln -s ${env_config}/bash_profile_mac .bash_profile
+ln -s ${env_config}/bashr_mac .bashrc
+ln -s ${env_config}/inputrc_mac .inputrc
 ln -s ${env_config}/git-completion.bash git-completion.bash
+ln -s ${env_config}/gitconfig .gitconfig
 
 echo "enjoy!"
