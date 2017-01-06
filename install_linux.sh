ba9671adc721b6740563ece230231c4369748056
Gavin Chou <gavineaglechou@gmail.com>
2017-01-06 14:51:22 +0800

    Add tmux.conf

--- install_linux.sh
+++ install_linux.sh
@@ -17,11 +17,13 @@ mv .bashrc .bashrc.`digitaldatetime`2>/dev/null
 mv .inputrc .inputrc.`digitaldatetime`2>/dev/null
 mv git-completion.bash git-completion.bash.`digitaldatetime`2>/dev/null
 mv .gitconfig .gitconfig.`digitaldatetime`2>/dev/null
+mv .tmux.conf .tmux.conf.`digitaldatetime`2>/dev/null
 
 ln -s ${env_config}/bash_profile_linux .bash_profile
 ln -s ${env_config}/bashrc_linux .bashrc
 ln -s ${env_config}/inputrc_linux .inputrc
 ln -s ${env_config}/git-completion.bash git-completion.bash
 ln -s ${env_config}/gitconfig .gitconfig
+ln -s ${env_config}/tmux.conf .tmux.conf
 
 echo "enjoy!"
--- install_mac.sh
+++ install_mac.sh
@@ -17,11 +17,13 @@ mv .bashrc .bashrc.`digitaldatetime`2>/dev/null
 mv .inputrc .inputrc.`digitaldatetime`2>/dev/null
 mv git-completion.bash git-completion.bash.`digitaldatetime`2>/dev/null
 mv .gitconfig .gitconfig.`digitaldatetime`2>/dev/null
+mv .tmux.conf .tmux.conf.`digitaldatetime`2>/dev/null
 
 ln -s ${env_config}/bash_profile_mac .bash_profile
 ln -s ${env_config}/bashr_mac .bashrc
 ln -s ${env_config}/inputrc_mac .inputrc
 ln -s ${env_config}/git-completion.bash git-completion.bash
 ln -s ${env_config}/gitconfig .gitconfig
+ln -s ${env_config}/tmux.conf .tmux.conf
 
 echo "enjoy!"
