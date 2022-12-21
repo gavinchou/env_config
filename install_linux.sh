3654b554882f538b91169af51dcfa7a931f8ae20
Gavin Chou <gavineaglechou@gmail.com>
2022-12-21 23:12:41 +0800

    Update zsh related env config

--- install_linux.sh
+++ install_linux.sh
@@ -6,24 +6,26 @@
 ## @date   2016-06-03-Fri
 
 function digitaldatetime() {
-  echo `date +"%Y%m%d%H%M%S"`
+	date +"%Y%m%d%H%M%S"
 }
 
 env_config=`pwd -P`
 cd $HOME
 # backup
-mv .bash_profile .bash_profile.`digitaldatetime`2>/dev/null
-mv .bashrc .bashrc.`digitaldatetime`2>/dev/null
-mv .inputrc .inputrc.`digitaldatetime`2>/dev/null
-mv git-completion.bash git-completion.bash.`digitaldatetime`2>/dev/null
-mv .gitconfig .gitconfig.`digitaldatetime`2>/dev/null
-mv .tmux.conf .tmux.conf.`digitaldatetime`2>/dev/null
+# mv .bash_profile .bash_profile.$(digitaldatetime) 2>/dev/null
+# mv .bashrc .bashrc.$(digitaldatetime) 2>/dev/null
+# mv .git-completion.bash .git-completion.bash.$(digitaldatetime) 2>/dev/null
+mv .inputrc .inputrc.$(digitaldatetime) 2>/dev/null
+mv .gitconfig .gitconfig.$(digitaldatetime) 2>/dev/null
+mv .tmux.conf .tmux.conf.$(digitaldatetime) 2>/dev/null
+mv .zshrc .zshrc.$(digitaldatetime) 2>/dev/null
 
-ln -s ${env_config}/bash_profile_linux .bash_profile
-ln -s ${env_config}/bashrc_linux .bashrc
+# ln -s ${env_config}/bash_profile_linux .bash_profile
+# ln -s ${env_config}/bashrc_linux .bashrc
+# ln -s ${env_config}/git-completion.bash .git-completion.bash
 ln -s ${env_config}/inputrc_linux .inputrc
-ln -s ${env_config}/git-completion.bash git-completion.bash
 ln -s ${env_config}/gitconfig .gitconfig
 ln -s ${env_config}/tmux.conf .tmux.conf
+ln -s ${env_config}/zshrc_linux .zshrc
 
 echo "enjoy!"
